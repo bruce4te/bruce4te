9cc8664103fffc2137577fbd2cb949e022fa4635
Daniel Schley <schley.daniel@gmail.com>
2018-11-23 13:12:42 +0100

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const colors = {
+  background: '#FFFFFF',
+  backgroundDark: '#f0e6f6',
+
+  primary: '#464547',
+  primaryLight: '#ae66df',
+  primaryDark: '#464547',
+
+  secondary: '#ff4081',
+  secondaryLight: '#ff79b0',
+  secondaryDark: '#c60055',
+};
+
+module.exports = {
+  colors,
+};
